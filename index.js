1a97ae32b4db7082798704062351df41ebbd3f09
Ronan-Codes <galv.ronan@gmail.com>
2022-03-29 23:17:38 -0400

    Optimize JS and remove carousel functions for now

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
-const gap = 250;
-
 const domSelectors = {
-  carousel: document.getElementById("carousel"),
   content: document.getElementById("content"),
-  next: document.getElementById("next"),
-  prev: document.getElementById("prev"),
   searchInput: document.getElementById("searchInput"),
   loader: document.querySelector(".loader-container"),
   searchForm: document.getElementById("searchForm")
@@ -12,45 +7,12 @@ const domSelectors = {
 
 const loaderTemplate = `<div id="loader" class="loader"></div>`
 
-let width = domSelectors.carousel.offsetWidth;
-
-function scrollEvents() {
-    domSelectors.next.addEventListener("click", e => {
-      domSelectors.carousel.scrollBy(width + gap, 0);
-      if (domSelectors.carousel.scrollWidth !== 0) {
-        domSelectors.prev.style.display = "flex";
-      }
-      if (domSelectors.content.scrollWidth - domSelectors.carousel.scrollLeft <= 1000) {
-        domSelectors.next.style.display = "none";
-      }
-    
-      console.log("scrollWidth",domSelectors.content.scrollWidth)
-      console.log("scrollLeft",domSelectors.carousel.scrollLeft)
-    });
-  
-
-  
-    domSelectors.prev.addEventListener("click", e => {
-      domSelectors.carousel.scrollBy(-(width + gap), 0);
-      if (domSelectors.carousel.scrollLeft - width - gap <= 0) {
-        domSelectors.prev.style.display = "none";
-      }
-      if (!domSelectors.content.scrollWidth - width - gap <= domSelectors.carousel.scrollLeft + width) {
-        domSelectors.next.style.display = "flex";
-      }
-    });
-}
-
-// 
-
 function fetchAlbum(artist) {
     return fetch(`https://itunes.apple.com/search?term=${artist}&media=music&entity=album&attribute=artistTerm&limit=200`)
         .then(res => res.json())
 }
 
-
-
-function createTemplateFromAlbumArr(albumArr, count){
+function createTemplateFromAlbumArr(albumArr){
     return albumArr.map(album => {
       return `<div class="item">
       <img src="${album.artworkUrl100}" class="item__img" aria-label="album image">
@@ -60,59 +22,74 @@ function createTemplateFromAlbumArr(albumArr, count){
   }).join(" ")
 }
 
-function renderTemplate(element, template) {
-  element.innerHTML = template
+function renderTemplate(element, albumArr) {
+    element.innerHTML = createTemplateFromAlbumArr(albumArr)
 }
 
-
-
-scrollEvents()
-
-// onchange event for input (Utilize later and replace onSubmit)
 function searchEvent() {
+    // inputOnChange()
+    formOnSubmit()
+}
+
+// function inputOnChange(){
+//     domSelectors.searchInput.addEventListener('change', (event) => {
+//         event.preventDefault()
     
-    domSelectors.searchInput.addEventListener('change', (event) => {
+//         displayLoading()
+    
+//         fetchAlbum(getInputValue()).then(albumJson => {
+//             const albums = albumJson.results
+//             const albumCount = albumJson.resultCount
+
+//             renderTemplate(domSelectors.content, createTemplateFromAlbumArr(albums, albumCount))
+//         })
+//         .then(hideLoading())
+//     });
+// }
+
+function formOnSubmit() {
+    domSelectors.searchForm.addEventListener('submit', (event) => {
         event.preventDefault()
+        let inputValue = getChildInputValue(event.target)
+        let artistName = inputValue? inputValue : null
     
-        const artistName = domSelectors.searchInput.value
-        const reformattedName = artistName.replaceAll(' ', '+')
-        console.log(reformattedName)
-    
-        displayLoading()
-    
-        fetchAlbum(reformattedName).then(albumJson => {
+        if (!artistName) {
+            alert("Please enter an artist and try again!")
+        } else {
+            displayLoading()
+
+            fetchAlbum(artistName).then(albumJson => {
             const albums = albumJson.results
             const albumCount = albumJson.resultCount
-            console.log(albums, albumCount)
-            renderTemplate(domSelectors.content, createTemplateFromAlbumArr(albums, albumCount))
-        })
-        .then(result => hideLoading())
+
+            if (!albumCount) {
+                alert("No albums found. Please try a different artist.")
+            }
+            renderTemplate(domSelectors.content, albums)
+            })
+            .catch((error) => {
+                alert(`${error}`);
+            })
+            .finally(() => {
+                hideLoading()
+            })
+        }
+
+        
     });
 }
-searchEvent()
 
+// function getInputValue() {
+//     const reformattedArtistName = domSelectors.searchInput.value.replaceAll(' ', '+')
+//     return reformattedArtistName
+// }
 
-// Optimize later with on change.. AND event delegation
-domSelectors.searchForm.addEventListener('submit', (event) => {
-    event.preventDefault()
-
-    const artistName = domSelectors.searchInput.value
-    const reformattedName = artistName.replaceAll(' ', '+')
-    console.log(reformattedName)
-
-    displayLoading()
-    console.log(domSelectors.loader.classList)
-
-    fetchAlbum(reformattedName).then(albumJson => {
-        const albums = albumJson.results
-        const albumCount = albumJson.resultCount
-        console.log(albums, albumCount)
-        renderTemplate(domSelectors.content, createTemplateFromAlbumArr(albums, albumCount))
-    })
-    .then(result => hideLoading())
-});
-
-// make function to show count 
+function getChildInputValue(parentElement){
+    // let artistName = parentElement.firstChild.nextSibling.value
+    let reformattedArtistName  = parentElement.firstChild.nextSibling.value.replaceAll(' ', '+')
+    parentElement.firstChild.nextSibling.value = ""
+    return reformattedArtistName
+}
 
 function displayLoading() {
     domSelectors.loader.classList.add("display");
@@ -121,3 +98,5 @@ function displayLoading() {
 function hideLoading() {
     domSelectors.loader.classList.remove("display");
 }
+
+searchEvent()
